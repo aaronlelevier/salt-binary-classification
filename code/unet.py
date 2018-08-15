7724a5da4af920266f4801562c7545d5543da024
aaronlelevier <aaron.lelevier@gmail.com>
2018-08-15 15:40:38 -0700

    tf unet code, keras unet code with data augmentation
    unet.py script - not fully tested

--- code/unet.py
+++ code/unet.py
@@ -1,36 +1,239 @@
-""" Convolutional Neural Network.
-
-Build and train a convolutional neural network with TensorFlow.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
 """
+Simple U-Net implementation in TensorFlow
+
+Credit: https://github.com/kkweon/UNet-in-Tensorflow
+
+Objective: detect vehicles
+
+y = f(X)
+
+X: image (640, 960, 3)
+y: mask (640, 960, 1)
+   - binary image
+   - background is masked 0
+   - vehicle is masked 255
+
+Loss function: maximize IOU
+
+    (intersection of prediction & grount truth)
+    -------------------------------
+    (union of prediction & ground truth)
+
+Notes:
+    In the paper, the pixel-wise softmax was used.
+    But, I used the IOU because the datasets I used are
+    not labeled for segmentations
+
+Original Paper:
+    https://arxiv.org/abs/1505.04597
+"""
+import time
+import os
+import pandas as pd
 import tensorflow as tf
-import numpy as np
 
 
-# Create some wrappers for simplicity
-def conv2d(x, W, b, strides=1):
-    # Conv2D wrapper, with bias and relu activation
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x)
+def image_augmentation(image, mask):
+    """Returns (maybe) augmented images
+
+    (1) Random flip (left <--> right)
+    (2) Random flip (up <--> down)
+    (3) Random brightness
+    (4) Random hue
+
+    Args:
+        image (3-D Tensor): Image tensor of (H, W, C)
+        mask (3-D Tensor): Mask image tensor of (H, W, 1)
+
+    Returns:
+        image: Maybe augmented image (same shape as input `image`)
+        mask: Maybe augmented mask (same shape as input `mask`)
+    """
+    concat_image = tf.concat([image, mask], axis=-1)
+
+    maybe_flipped = tf.image.random_flip_left_right(concat_image)
+    maybe_flipped = tf.image.random_flip_up_down(concat_image)
+
+    image = maybe_flipped[:, :, :-1]
+    mask = maybe_flipped[:, :, -1:]
+
+    image = tf.image.random_brightness(image, 0.7)
+    image = tf.image.random_hue(image, 0.3)
+
+    return image, mask
 
 
-def maxpool2d(x, k=2):
-    # MaxPool2D wrapper
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                          padding='SAME')
+def get_image_mask(queue, augmentation=True):
+    """Returns `image` and `mask`
+
+    Input pipeline:
+        Queue -> CSV -> FileRead -> Decode JPEG
+
+    (1) Queue contains a CSV filename
+    (2) Text Reader opens the CSV
+        CSV file contains two columns
+        ["path/to/image.jpg", "path/to/mask.jpg"]
+    (3) File Reader opens both files
+    (4) Decode JPEG to tensors
+
+    Notes:
+        height, width = 640, 960
+
+    Returns
+        image (3-D Tensor): (640, 960, 3)
+        mask (3-D Tensor): (640, 960, 1)
+    """
+    text_reader = tf.TextLineReader(skip_header_lines=1)
+    _, csv_content = text_reader.read(queue)
+
+    image_path, mask_path = tf.decode_csv(
+        csv_content, record_defaults=[[""], [""]])
+
+    image_file = tf.read_file(image_path)
+    mask_file = tf.read_file(mask_path)
+
+    image = tf.image.decode_jpeg(image_file, channels=3)
+    image.set_shape([640, 960, 3])
+    image = tf.cast(image, tf.float32)
+
+    mask = tf.image.decode_jpeg(mask_file, channels=1)
+    mask.set_shape([640, 960, 1])
+    mask = tf.cast(mask, tf.float32)
+    mask = mask / (tf.reduce_max(mask) + 1e-7)
+
+    if augmentation:
+        image, mask = image_augmentation(image, mask)
+
+    return image, mask
 
 
-def deconv2d(x, W, b, stride=2):
-    # conv2d_transpose wrapper
-    x_shape = tf.shape(x)
-    output_shape = tf.stack([x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]//2])
-    return tf.nn.conv2d_transpose(
-        x, W, output_shape, strides=[1, stride, stride, 1])
+def conv_conv_pool(input_,
+                   n_filters,
+                   training,
+                   flags,
+                   name,
+                   pool=True,
+                   activation=tf.nn.relu):
+    """{Conv -> BN -> RELU}x2 -> {Pool, optional}
+
+    Args:
+        input_ (4-D Tensor): (batch_size, H, W, C)
+        n_filters (list): number of filters [int, int]
+        training (1-D Tensor): Boolean Tensor
+        name (str): name postfix
+        pool (bool): If True, MaxPool2D
+        activation: Activaion functions
+
+    Returns:
+        net: output of the Convolution operations
+        pool (optional): output of the max pooling operations
+    """
+    net = input_
+
+    with tf.variable_scope("layer{}".format(name)):
+        for i, F in enumerate(n_filters):
+            net = tf.layers.conv2d(
+                net,
+                F, (3, 3),
+                activation=None,
+                padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(flags.reg),
+                name="conv_{}".format(i + 1))
+            net = tf.layers.batch_normalization(
+                net, training=training, name="bn_{}".format(i + 1))
+            net = activation(net, name="relu{}_{}".format(name, i + 1))
+
+        if pool is False:
+            return net
+
+        pool = tf.layers.max_pooling2d(
+            net, (2, 2), strides=(2, 2), name="pool_{}".format(name))
+
+        return net, pool
+
+
+def upconv_concat(inputA, input_B, n_filter, flags, name):
+    """Upsample `inputA` and concat with `input_B`
+
+    Args:
+        input_A (4-D Tensor): (N, H, W, C)
+        input_B (4-D Tensor): (N, 2*H, 2*H, C2)
+        name (str): name of the concat operation
+
+    Returns:
+        output (4-D Tensor): (N, 2*H, 2*W, C + C2)
+    """
+    up_conv = upconv_2D(inputA, n_filter, flags, name)
+
+    return tf.concat(
+        [up_conv, input_B], axis=-1, name="concat_{}".format(name))
+
+
+def upconv_2D(tensor, n_filter, flags, name):
+    """Up Convolution `tensor` by 2 times
+
+    Args:
+        tensor (4-D Tensor): (N, H, W, C)
+        n_filter (int): Filter Size
+        name (str): name of upsampling operations
+
+    Returns:
+        output (4-D Tensor): (N, 2 * H, 2 * W, C)
+    """
+
+    return tf.layers.conv2d_transpose(
+        tensor,
+        filters=n_filter,
+        kernel_size=2,
+        strides=2,
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(flags.reg),
+        name="upsample_{}".format(name))
+
+class Flags:
+    reg = 0.1
+
+
+def make_unet(X, training, flags=None):
+    """Build a U-Net architecture
+
+    Args:
+        X (4-D Tensor): (N, H, W, C)
+        training (1-D Tensor): Boolean Tensor is required for batchnormalization layers
+
+    Returns:
+        output (4-D Tensor): (N, H, W, C)
+            Same shape as the `input` tensor
+
+    Notes:
+        U-Net: Convolutional Networks for Biomedical Image Segmentation
+        https://arxiv.org/abs/1505.04597
+    """
+    conv1, pool1 = conv_conv_pool(X, [8, 8], training, flags, name=1)
+    conv2, pool2 = conv_conv_pool(pool1, [16, 16], training, flags, name=2)
+    conv3, pool3 = conv_conv_pool(pool2, [32, 32], training, flags, name=3)
+    conv4, pool4 = conv_conv_pool(pool3, [64, 64], training, flags, name=4)
+    conv5 = conv_conv_pool(
+        pool4, [128, 128], training, flags, name=5, pool=False)
+
+    up6 = upconv_concat(conv5, conv4, 64, flags, name=6)
+    conv6 = conv_conv_pool(up6, [64, 64], training, flags, name=6, pool=False)
+
+    up7 = upconv_concat(conv6, conv3, 32, flags, name=7)
+    conv7 = conv_conv_pool(up7, [32, 32], training, flags, name=7, pool=False)
+
+    up8 = upconv_concat(conv7, conv2, 16, flags, name=8)
+    conv8 = conv_conv_pool(up8, [16, 16], training, flags, name=8, pool=False)
+
+    up9 = upconv_concat(conv8, conv1, 8, flags, name=9)
+    conv9 = conv_conv_pool(up9, [8, 8], training, flags, name=9, pool=False)
+
+    return tf.layers.conv2d(
+        conv9,
+        1, (1, 1),
+        name='final',
+        activation=tf.nn.sigmoid,
+        padding='same')
+
 
 # hyper parameters
 epochs = 2
@@ -40,75 +243,7 @@ batch_size = 32
 X = tf.placeholder(tf.float32, [None, 128, 128, 1])
 Y = tf.placeholder(tf.float32, [None, 128, 128, 1])
 
-# weights & biases
-weights = {
-    'w1': tf.Variable(tf.random_normal([3, 3, 1, 16])),
-    'w2': tf.Variable(tf.random_normal([3, 3, 16, 32])),
-    'w3': tf.Variable(tf.random_normal([3, 3, 32, 64])),
-    'w4': tf.Variable(tf.random_normal([3, 3, 64, 128])),
-    'w5': tf.Variable(tf.random_normal([3, 3, 128, 256])),
-    'w6': tf.Variable(tf.random_normal([3, 3, 256, 128])),
-    'w7': tf.Variable(tf.random_normal([3, 3, 128, 64])),
-    'w8': tf.Variable(tf.random_normal([3, 3, 64, 32])),
-    'w9': tf.Variable(tf.random_normal([3, 3, 32, 16])),
-    'w10': tf.Variable(tf.random_normal([3, 3, 16, 1])),
-}
-
-biases = {
-    # conv
-    'b1': tf.Variable(tf.random_normal([16])),
-    'b2': tf.Variable(tf.random_normal([32])),
-    'b3': tf.Variable(tf.random_normal([64])),
-    'b4': tf.Variable(tf.random_normal([128])),
-    # middle
-    'b5': tf.Variable(tf.random_normal([256])),
-    # deconv
-    'b6': tf.Variable(tf.random_normal([128])),
-    'b7': tf.Variable(tf.random_normal([64])),
-    'b8': tf.Variable(tf.random_normal([32])),
-    'b9': tf.Variable(tf.random_normal([16])),
-    'b10': tf.Variable(tf.random_normal([1])),
-}
-
-def conv_net(x, weights, bias):
-    # 128 -> 64
-    conv1 = conv2d(x, weights['w1'], biases['b1'])
-    conv1 = maxpool2d(conv1)
-    
-    # 64 -> 32
-    conv2 = conv2d(conv1, weights['w2'], biases['b2'])
-    conv2 = maxpool2d(conv2)
-
-    # 32 -> 16
-    conv3 = conv2d(conv2, weights['w3'], biases['b3'])
-    conv3 = maxpool2d(conv3)
-
-    # 16 -> 8
-    conv4 = conv2d(conv3, weights['w4'], biases['b4'])
-    conv4 = maxpool2d(conv4)
-
-    # middle
-    convm = conv2d(conv4, weights['w5'], biases['b5'])
-
-    # 8 -> 16
-    deconv4 = deconv2d(convm, weights['w6'], biases['b6'])
-
-    # 16 -> 32
-    deconv3 = deconv2d(deconv4, weights['w7'], biases['b7'])
-
-    # 32 -> 64
-    deconv2 = deconv2d(deconv3, weights['w8'], biases['b8'])
-
-    # 64 -> 128
-    deconv1 = deconv2d(deconv2, weights['w9'], biases['b9'])
-
-    # flatten filters for final output
-    # (None, 128, 128, 16) -> (None, 128, 128, 1)
-    out = deconv2d(deconv1, weights['w10'], biases['b10'])
-    return out
-
-
-logits = conv_net(X, weights, biases)
+logits = make_unet(X, training=tf.constant(True))
 prediction = tf.nn.softmax(logits)
 
 # Define loss and optimizer
